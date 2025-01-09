dd5295efb9a4930fc18d647ee0e14cd8b6e9f1ef
Ezat <ezatelbery164@gmail.com>
2025-01-09 16:18:40 +0200

    add change

--- server/index.js
+++ server/index.js
@@ -24,6 +24,7 @@ app.use(cors());
 app.options("*", cors());
 app.use(compression());
 app.use("./uploads", express.static("uploads"));
+app.use(express.static(__dirname));
 
 app.post(
   "/webhook-checkout",
